fad778b287f243622715ba4e4a70fb957b60aad1
Shahin Raza <shahin.raza@webaccessglobal.com>
2019-12-30 12:44:10 +0530

    second commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { FormGroup } from '@angular/forms';
 import { NavController } from '@ionic/angular';
+// import { NavController, ToastController, Events } from 'ionic-angular';
+import { StatisticsPage } from '../statistics/statistics.page';
+// import { Calendar } from '@ionic-native/calendar/ngx';
+
 
 
 
@@ -16,13 +20,19 @@ export class HomePage {
   constructor(
     public storage: Storage,
     public navCtrl: NavController,
+    // private calendar: Calendar
     ) {
-    
+      // this.calendar.createCalendar('MyCalendar').then(
+      //   (msg) => { console.log(msg); },
+      //   (err) => { console.log(err); }
+      // );
   }
 
   dailyStuffForm(form) {
     console.log(form.value);
-    this.storage.set('Stuff Entry', form.value); 
+    this.storage.set('Stuff Entry', form.value);
+    // this.navCtrl.push(StatisticsPage);
+    // this.navCtrl.navigateRoot('/statistics');
   }
 
   getStuff(type) {
@@ -33,14 +43,14 @@ export class HomePage {
   }
 
   
-  onStuff(type) {
-    console.log('type*--',type);
-    this.stuffData = type;
-    this.storage.set('stuffType', this.stuffData);
-    this.storage.get('stuffType').then(data=>{
-      console.log('stdata ',data);
-    });
-    // this.navCtrl.push(StatisticsPage);
+  // onStuff(type) {
+  //   console.log('type*--',type);
+  //   this.stuffData = type;
+  //   this.storage.set('stuffType', this.stuffData);
+  //   this.storage.get('stuffType').then(data=>{
+  //     console.log('stdata ',data);
+  //   });
+  //   // this.navCtrl.push(StatisticsPage);
 
-  }
+  // }
 }
